41c19aae4bec0866bb7439d59a8d72433a4dd071
Mario-King-64 <104554253+Mario-King-64@users.noreply.github.com>
2023-12-07 11:23:14 +0100

    Updated movement (Not done)

--- Map/Logo.gd
+++ Map/Logo.gd
@@ -1,25 +1,50 @@
 extends Sprite2D
 
-var pos: Vector2 = Vector2.ZERO
-const speed: int = 128	
-var test_scale: int = 1
+@onready var Player = $Logo
+var animation_speed = 3
+var moving = false
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pos = Vector2(512, 512)
-	position = pos
-	
-#	var test_rotation = 45
-#	rotation_degrees = test_rotation
-	
-#	test_scale = 2
-#	scale = Vector2(test_scale, test_scale)
+	position = position.snapped(Vector2.ONE * tile_size)
+	print(position)
+	position = Vector2.ONE * tile_size
+	print(position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pos.x += delta
-	pos.y += delta
-	position = pos
-	
-#	test_scale += 1
-#	scale = Vector2(test_scale, test_scale)
+	if position.x > 960:
+		position.x = 64
+
+	if position.x < 64:
+		position.x = 960
+
+	if position.y > 960:
+		position.y = 64
+
+	if position.y < 64:
+		position.y = 960
+
+func _unhandled_input(event):
+	if moving:
+		return
+	for direction in inputs.keys():
+		if event.is_action_pressed(direction):
+			move(direction)
+
+func move(direction):
+	Player.target_position = inputs[direction] * tile_size
+	Player.force_raycast_update()
+	if !Player.is_colliding():
+		#position += inputs[dir] * tile_size
+		var tween = create_tween()
+		tween.tween_property(self, "position",
+			position + inputs[direction] *    tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
--- Map/map.gd
+++ Map/map.gd
@@ -1,47 +1,7 @@
 extends Node2D
 
-var test_array: Array[String] = ["Test", "Hello", "Stuff"]
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$Logo.rotation_degrees = 0 ##Use 90 for testing
-	
-	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
-	
-#	for i in test_array:
-#		print(i)
-#	print(test_array[0])
+	rotate(0)
 
 func _process(delta):
-#	$Logo.rotation_degrees += 100 * delta
-	
-	##Flow -> if rotation > 245 -> 0
-#	if $Logo.rotation_degrees > 245:
-#		$Logo.rotation_degrees = 0
-
-	if $Logo.position.x > 1024:
-		$Logo.pos.x = 0
-	
-	if $Logo.position.x < 0:
-		$Logo.pos.x = 1024
-	
-	if $Logo.position.y > 1024:
-		$Logo.pos.y = 0
-	
-	if $Logo.position.y < 0:
-		$Logo.pos.y = 1024
-	
-var tile_size = 64
-var inputs = {"right": Vector2.RIGHT,
-			"left": Vector2.LEFT,
-			"up": Vector2.UP,
-			"down": Vector2.DOWN}
-
-func _unhandled_input(event):
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
-
-func move(dir):
-	position += inputs[dir] * tile_size
+	rotate(0)
